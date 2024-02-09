959acb85ada189406db0e79689a90fabc12662e6
Daniel de Sousa <156535206+DanielDeSousaDEV@users.noreply.github.com>
2024-02-09 19:43:05 -0300

    Atualizar o Index.php

--- Estruturador-de-Query's/Index.php
+++ Estruturador-de-Query's/Index.php
@@ -1,1 +1,30 @@
-j
+<pre>
+<?php
+  function createSelectQuery(string $tbName, $search = null){
+      $queryInit ='SELECT * FROM ' . $tbName;
+      
+      if($search === null){
+          return $queryInit;
+      };
+      
+      $query = $queryInit . ' WHERE ';
+    
+      foreach($search as $key => $value){
+        $queryFin .= $key . ' = ' . $value;
+      };
+      
+      $query = $query . $queryFin;
+    
+      return $query;
+  };
+  
+  $tbNameTest = 'tb_aluno';
+
+  $searchTest = [
+      'id_aluno' => '07',
+  ];
+  
+  $testeFunc = createSelectQuery($tbNameTest, $searchTest);
+  var_dump($testeFunc);
+?>
+</pre>
