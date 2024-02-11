c66f5f27b11e4c31de093169b0f432239585e1ef
Daniel de Sousa <156535206+DanielDeSousaDEV@users.noreply.github.com>
2024-02-11 09:17:21 -0300

    Atualizar o Index.php

--- Estruturador-de-Query's/Index.php
+++ Estruturador-de-Query's/Index.php
@@ -7,21 +7,28 @@
           return $query;
       };
       
-      $query .= ' WHERE ';
-    
+      //essa falta de espaço na linha abaixo serve para que no foreach eu possa adicionar espaços tamnto no inicio quanto no fim da string
+      $query .= ' WHERE';
+      
       foreach($search as $key => $value){
-        $queryFin .= $key . ' = ' . $value;
+        $queryFin .= ' ' . $key . ' = ' . $value . ' ,';
       };
       
+      $regex = '/[ ][,]$/';
+      $replace = ';';
+      $subject = $queryFin;
+
+      $queryFin = preg_replace($regex,$replace,$subject);
+    
       $query = $query . $queryFin;
     
       return $query;
   };
   
   $tbNameTest = 'tb_aluno';
-
   $searchTest = [
       'id_aluno' => '07',
+      'nome_aluno' => 'Daniel'
   ];
   
   $testeFunc = createSelectQuery($tbNameTest, $searchTest);
