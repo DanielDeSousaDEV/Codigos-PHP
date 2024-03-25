1eb5a3d4f712238f07d10f5d8c9ced76be89f70f
Daniel de Sousa <156535206+DanielDeSousaDEV@users.noreply.github.com>
2024-03-25 16:39:21 -0300

    Update Index.php

--- Estruturador-de-Query's/Index.php
+++ Estruturador-de-Query's/Index.php
@@ -1,7 +1,8 @@
 <pre>
 <?php
+  //SELECT -> READ (R)
   function createSelectQuery(string $tbName, $search = null){
-      $query ='SELECT * FROM ' . $tbName;
+      $query = 'SELECT * FROM ' . $tbName;
       
       if($search === null){
           return $query;
@@ -17,12 +18,57 @@
       $replace = ';';
       $subject = $queryFin;
 
-      $queryFin = preg_replace($regex,$replace,$subject);
+      $queryFin = preg_replace($regex, $replace, $subject);
     
       $query = $query . $queryFin;
     
       return $query;
   };
+
+  //INSERT -> CREATE (C)
+  function createInsertQuery(string $tbName, ...$values){
+      $query = 'INSERT INTO ' . $tbName;
+    
+      //não sei como escreve a query de insert
+      //insert into tbname (value1,value2,value3)
+      //pode passar o id como null se for auto-increment
+  }
+
+
+  //DELETE -> DELETE (D)
+  function createDeleteQuery(string $tbName, $conditions = null){
+      $query = 'DELETE * FROM ' . $tbName;
+      
+      if($conditions === null){
+          return $query;//COM COMCATENAR COM PONTO VIRGULA
+      };
+    
+      $query .= ' WHERE ';
+      
+      foreach($conditions as $key => $value){
+        $queryFin .= $key . ' = ' . $value . ' , ';
+      };
+      
+      $regex = '/[ ][,][ ]$/';
+      $replace = ';';
+      $subject = $queryFin;
+    
+      $queryFin = preg_replace($regex, $replace, $subject);
+    
+      $query = $query . $queryFin;
+    
+      return $query;
+    
+      //delete * from tbname where tbcamp = value;
+  }
+
+  //UPDATE -> UPDATE (U)
+  function createUpdateQuery(){
+      
+  }
+
+
+
   
   $tbNameTest = 'tb_aluno';
   $searchTest = [
@@ -30,7 +76,11 @@
       'nome_aluno' => 'Daniel'
   ];
   
-  $testeFunc = createSelectQuery($tbNameTest, $searchTest);
+  $testeFunc = createDeleteQuery($tbNameTest, $searchTest);
+
+  //$testeFunc = createSelectQuery($tbNameTest, $searchTest);
+
+  //VERIFICAR SE É UMA STRING
   var_dump($testeFunc);
 ?>
 </pre>
