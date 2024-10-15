4ffb95781563c2e594158cb7fefd6b03a01317ff
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2024-10-15 16:45:38 -0300

    estudos sobre closures

--- Closures/index.php
+++ Closures/index.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teste De Closure</title>
+    <style>
+        body{
+            margin: 0 1rem;
+        }
+    </style>
 </head>
 <body>
     <pre>
