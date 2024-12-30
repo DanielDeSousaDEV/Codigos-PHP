8b7cc5b5a0e783a21986c2ef06deb98ec1a5e146
Daniel de Sousa <156535206+DanielDeSousaDEV@users.noreply.github.com>
2024-12-30 09:41:10 -0300

    Atualizar o Index.php

--- Matrizes/Index.php
+++ Matrizes/Index.php
@@ -8,7 +8,7 @@
           3,4
       ]
   ];
-  
+
   $mtz2 = [
       [
           5,6
@@ -26,32 +26,37 @@
       $lineCont = 0;//contandor da linha
       $i;      //contador necessario para usar o metodo mtz1 linha X mtz2 coluna
       $b;      //contador da correspondencia contraria encontrada necessario para usar o metodo mtz1 linha X mtz2 coluna
-    
+
       //Verificar se a quantidade de colunas da 1° matriz bate com o numero de linhas da 2° matriz
       if(count($mtz1[0]) === count($mtz2)){
-        
-        foreach($mtz1 as $line){
-          for($b = 0; $b < count($mtz1); $b++){
+
+        for($line = 0 ; $line < count($mtz1) ;$line++){
+          for($b = 0; $b < count($mtz2[0]); $b++){
               //$res[∆][∆] = $mtz1[∆][∆] * $mtz2[∆][∆];
                                //b//i//      //i//b//
-            
+
             for($i = 0; $i < count($mtz2[$b]); $i++){
-                
-                $resLin = $line[$i] * $mtz2[$i][$b];
-                
+
+                $resLin = $mtz1[$line][$i] * $mtz2[$i][$b];
+
             };
-              $res[$lineCont][$b] = $resLin;
+            $res[$lineCont][$b] = $resLin;
+            $resLin = 0;
+            
           };
           $lineCont++;
         };
-        var_dump($res);
+        return $res;
       }else{
         return 'não é possível multiplicar as matrizes';
       };
   };
 
-  //Mostragem do dados
-  $res = mulMtz($mtz1, $mtz2);
-  //var_dump($res);
-
 ?>
+<pre>
+<?php
+  //mostragem dos dados
+  $res = mulMtz($mtz1, $mtz2);
+  var_dump($res);
+?>
+</pre>
